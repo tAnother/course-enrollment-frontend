bb0ec8b3a63845525d227554f449076f84b63baf
YT <tongfutago@hotmail.com>
2024-09-12 12:20:41 -0700

    Styling

--- src/components/CourseTable.js
+++ src/components/CourseTable.js
@@ -12,20 +12,20 @@ import Button from '@mui/material/Button';
 export default function CourseTable(props) {
   const enroll = (courseName) => {
     CourseService.enrollCourse(courseName)
-    .then(res => {
-      alert(`You've successfully enrolled in: ${courseName}`);
-    }).catch(err => {
-      alert(`Error: Enrollment failed due to: ${err.message}`);
-    });
+      .then(res => {
+        alert(`You've successfully enrolled in: ${courseName}`);
+      }).catch(err => {
+        alert(`Error: Enrollment failed due to: ${err.message}`);
+      });
   }
 
   const drop = (courseName) => {
     CourseService.dropCourse(courseName)
-    .then(res => {
-      alert(`You've successfully dropped: ${courseName}`);
-    }).catch(err => {
-      alert(`Error: Dropping failed due to: ${err.message}`);
-    });
+      .then(res => {
+        alert(`You've successfully dropped: ${courseName}`);
+      }).catch(err => {
+        alert(`Error: Dropping failed due to: ${err.message}`);
+      });
   }
 
   return (
@@ -42,28 +42,28 @@ export default function CourseTable(props) {
           </TableRow>
         </TableHead>
         <TableBody>
-        {props.courses.map((course, index) => {
-          return (
-          <TableRow
-          key={course.courseName}
-          sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-        >
-          <TableCell component="th" scope="row">{index+1}</TableCell>
-          <TableCell align="right">{course.courseName}</TableCell>
-          <TableCell align="right">{course.courseDescription}</TableCell>
-          <TableCell align="right">{course.courseLocation}</TableCell>
-          <TableCell align="right">{course.instructorId}</TableCell>
-          <TableCell align="right">
-            <Button 
-              variant="outlined"
-              style={{ color: course.isEnrolled ? "red" : "green" }}
-              onClick={() => course.isEnrolled ? drop(course.courseName) : enroll(course.courseName)}
-            >
-              {course.isEnrolled ? "Drop" : "Enroll"}
-            </Button>
-          </TableCell>
-        </TableRow>
-        )})}
+          {props.courses.map((course, index) => {
+            return (
+              <TableRow
+                key={course.courseName}
+                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+              >
+                <TableCell component="th" scope="row">{index+1}</TableCell>
+                <TableCell align="right">{course.courseName}</TableCell>
+                <TableCell align="right">{course.courseDescription}</TableCell>
+                <TableCell align="right">{course.courseLocation}</TableCell>
+                <TableCell align="right">{course.instructorId}</TableCell>
+                <TableCell align="right">
+                  <Button 
+                    variant="outlined"
+                    style={{ color: course.isEnrolled ? "red" : "green" }}
+                    onClick={() => course.isEnrolled ? drop(course.courseName) : enroll(course.courseName)}
+                  >
+                    {course.isEnrolled ? "Drop" : "Enroll"}
+                  </Button>
+                </TableCell>
+              </TableRow>
+            )})}
         </TableBody>
       </Table>
     </TableContainer>
