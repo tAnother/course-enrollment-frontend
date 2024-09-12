1f3db22242a8abf6e2ea96281f4d103e0439db4b
YT <tongfutago@hotmail.com>
2024-09-11 17:33:41 -0700

    Update enroll/drop

--- src/services/CourseService.js
+++ src/services/CourseService.js
@@ -8,9 +8,14 @@ export const CourseService = {
     return axios.get("/api/student/courses");
   },
   enrollCourse: (courseName) => {
-    return axios.post(`/api/student/course/${courseName}`);
+    return axios.post(`/api/student/course`, {
+      "courseName": courseName
+    });
   },
   dropCourse: (courseName) => {
-    return axios.delete(`/api/student/course/${courseName}`);
+    // note that axios.delete has a slightly different arg structure than axios.post
+    return axios.delete(`/api/student/course`, {
+      data: { courseName: courseName },
+    });
   },
 }
